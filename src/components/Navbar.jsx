21b690319b64b4f646fc2bd844fb8c279b36d12c
codenextdoor <xuluv911@gmail.com>
2022-12-20 12:10:23 +0545

    Designing Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,32 @@
 import React from "react";
-
+import { Search, ShoppingCartOutlined } from "@mui/icons-material";
+import { Badge } from "@mui/material";
 const Navbar = () => {
+  const style = "text-[14px] cursor-pointer ml-[25px]";
   return (
     <div className="navbar h-[60px] shadow-md relative z-10">
       <div className="wrapper pl-[20px] pr-[20px] pt-[10px] pb-[10px] flex justify-between items-center">
-        <div className="left flex flex-1 items-center ">1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className="left flex flex-1 items-center ">
+          <div className="language cursur-pointer text-[16px]">En</div>
+          <div className="searchInput flex border-[2px] border-solid border-lightgrey rounded-md items-center ml-[10px] p-[5px] focus-within:border-[#8a4af3] transition-all">
+            <input className="input outline-none" type="text" />
+            <Search className="cursor-pointer" style={{ fontSize: "18px" }} />
+          </div>
+        </div>
+        {/* logo */}
+        <div className="center flex-1 text-center">
+          <div className="logo font-bold text-lg">Summer Kings</div>
+        </div>
+        {/* right div */}
+        <div className="right flex flex-1 items-center justify-end">
+          <div className={style}>Register</div>
+          <div className={style}>Sign in</div>
+          <div>
+            <Badge badgeContent={2} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </div>
+        </div>
       </div>
     </div>
   );
