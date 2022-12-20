7aa6333ebd94f6af9f339ee8b2f363157cd27f97
codenextdoor <xuluv911@gmail.com>
2022-12-20 15:36:11 +0545

    Setting arrows commit

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,14 +1,22 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
 import React from "react";
 
 const Slider = () => {
+  const arrowStyle =
+    "rounded-full bg-grey flex justify-center items-center shadow-sm hover:cursor-pointer";
+
   return (
-    <div className="parentDiv">
+    <div className="parentDiv h-[540px] bg-white flex items-center justify-between">
       {/* leftarrow div */}
-      <div className="leftarrow"></div>
+      <div className={arrowStyle}>
+        <ArrowLeftOutlined style={{ fontSize: "50px" }} />
+      </div>
       {/* slide div */}
       <div className="slide"></div>
       {/* rightarrow div */}
-      <div className="rightarrow"></div>
+      <div className={arrowStyle}>
+        <ArrowRightOutlined style={{ fontSize: "50px" }} />
+      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Announce from "../components/Announce";
 import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       {" "}
       <Announce />
       <Navbar />
+      <Slider />
     </div>
   );
 };
