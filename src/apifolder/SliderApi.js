60d500bbbaf1e02c547509517f2ce0aa7a2147bf
codenextdoor <xuluv911@gmail.com>
2022-12-21 11:02:55 +0545

    Image SliderApi commit

---
+++ src/apifolder/SliderApi.js
@@ -0,0 +1,19 @@
+export const ApiSlides = [
+  {
+    src: "https://image.freepik.com/free-photo/happy-woman-shopping-with-shopping-bags_1150-20495.jpg",
+    content: {
+      h2: "Amazing Deals this Summer",
+      p: "Upto 40% off",
+    },
+    background: " bg-[#c6e400]",
+  },
+  {
+    src: "https://media.istockphoto.com/id/1193750118/photo/beautiful-asian-woman-carrying-colorful-bags-shopping-online-with-mobile-phone.jpg?s=612x612&w=0&k=20&c=j1SpSX7c3qzBrUT5f7HRoOfxQnPxZY_c6yb3AvXA5f8=",
+    active: true,
+    content: {
+      h2: "Christmas Deals this week",
+      p: "Upto 20% off",
+    },
+    background: " bg-[#A39CE0]",
+  },
+];
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,11 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
 import React from "react";
-
+import { ApiSlides } from "../apifolder/SliderApi";
 const Slider = () => {
+  // usestate hooks
+  const [slides] = useState(ApiSlides);
+  const [activeSlide, setActiveSlide] = useState(0);
+
   const arrowStyle =
     "rounded-full bg-grey flex justify-center items-center shadow-sm hover:cursor-pointer";
 
@@ -12,14 +16,19 @@ const Slider = () => {
         <ArrowLeftOutlined style={{ fontSize: "50px" }} />
       </div>
       {/* slide div */}
-      <div className="wrapper flex w-[100%] h-[500px] justify-center items-center shadow-2xl rounded-lg border-[#c0c0c0] border-10px overflow-hidden relative">
+      <div className="wrapper flex w-[100%] h-[500px] justify-center items-center shadow-2xl rounded-lg border-[#c0c0c0] border-10px overflow-hidden relative bg-[#A39CE0]">
         <div className="slide flex items-center justify-center h-[100%] ">
-          <div className="forImage flex flexx-1 justify-center items-center h-[100%]">
+          <div className="forImage flex flex-1 justify-center items-center h-[100%]">
             <img
               className="h-[100%] object-cover"
               src="https://media.istockphoto.com/id/1193750118/photo/beautiful-asian-woman-carrying-colorful-bags-shopping-online-with-mobile-phone.jpg?s=612x612&w=0&k=20&c=j1SpSX7c3qzBrUT5f7HRoOfxQnPxZY_c6yb3AvXA5f8="
               alt="slide_img"
             />
+          </div>
+          <div className="des flex flex-col flex-1 place-items-start justify-center ml-11">
+            <h2 className="text-[55px]">Hey there is an awesome deal today</h2>
+            <p className="text-[30px]">Upto 40% off</p>
+            <button className="btn mt-[10px]">Shop now</button>
           </div>
         </div>
       </div>
