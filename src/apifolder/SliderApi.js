2bff68b1f8071e1d40a955235d6d0b5fe3f65934
codenextdoor <xuluv911@gmail.com>
2022-12-21 14:01:37 +0545

    slides animations

--- src/apifolder/SliderApi.js
+++ src/apifolder/SliderApi.js
@@ -11,7 +11,7 @@ export const ApiSlides = [
     src: "https://media.istockphoto.com/id/1193750118/photo/beautiful-asian-woman-carrying-colorful-bags-shopping-online-with-mobile-phone.jpg?s=612x612&w=0&k=20&c=j1SpSX7c3qzBrUT5f7HRoOfxQnPxZY_c6yb3AvXA5f8=",
     active: true,
     content: {
-      h2: "Christmas Deals this week",
+      h2: "Sunglasses Deals this week",
       p: "Upto 20% off",
     },
     background: " bg-[#A39CE0]",
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,8 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
-import React from "react";
+import { React, useState } from "react";
 import { ApiSlides } from "../apifolder/SliderApi";
+
+import "./Slider.css";
 const Slider = () => {
   // usestate hooks
   const [slides] = useState(ApiSlides);
@@ -9,32 +11,59 @@ const Slider = () => {
   const arrowStyle =
     "rounded-full bg-grey flex justify-center items-center shadow-sm hover:cursor-pointer";
 
+  const nextSlide = () => {
+    if (activeSlide === slides.length - 1) {
+      setActiveSlide(0);
+    } else {
+      setActiveSlide(activeSlide + 1);
+    }
+  };
+
+  const prevSlide = () => {
+    if (activeSlide === 0) {
+      setActiveSlide(slides.length - 1);
+    } else {
+      setActiveSlide(activeSlide - 1);
+    }
+  };
+
   return (
     <div className="parentDiv h-[540px] bg-white flex items-center justify-between">
       {/* leftarrow div */}
       <div className={arrowStyle}>
-        <ArrowLeftOutlined style={{ fontSize: "50px" }} />
+        <ArrowLeftOutlined style={{ fontSize: "50px" }} onClick={prevSlide} />
       </div>
       {/* slide div */}
-      <div className="wrapper flex w-[100%] h-[500px] justify-center items-center shadow-2xl rounded-lg border-[#c0c0c0] border-10px overflow-hidden relative bg-[#A39CE0]">
-        <div className="slide flex items-center justify-center h-[100%] ">
-          <div className="forImage flex flex-1 justify-center items-center h-[100%]">
-            <img
-              className="h-[100%] object-cover"
-              src="https://media.istockphoto.com/id/1193750118/photo/beautiful-asian-woman-carrying-colorful-bags-shopping-online-with-mobile-phone.jpg?s=612x612&w=0&k=20&c=j1SpSX7c3qzBrUT5f7HRoOfxQnPxZY_c6yb3AvXA5f8="
-              alt="slide_img"
-            />
-          </div>
-          <div className="des flex flex-col flex-1 place-items-start justify-center ml-11">
-            <h2 className="text-[55px]">Hey there is an awesome deal today</h2>
-            <p className="text-[30px]">Upto 40% off</p>
-            <button className="btn mt-[10px]">Shop now</button>
-          </div>
-        </div>
-      </div>
+      {slides.map((slide, index) => {
+        if (index === activeSlide) {
+          return (
+            <div
+              className={
+                `wrapper flex w-[100%] h-[500px] justify-center items-center shadow-2xl rounded-lg border-[#c0c0c0] border-10px overflow-hidden relative` +
+                slide.background
+              }
+            >
+              <div className="slide flex items-center justify-center h-[100%] ">
+                <div className="forImage flex flex-1 justify-center items-center h-[100%]">
+                  <img
+                    className="h-[100%] object-cover"
+                    src={slide.src}
+                    alt="slide_img"
+                  />
+                </div>
+                <div className="des flex flex-col flex-1 place-items-start justify-center ml-11">
+                  <h2 className="text-[55px]">{slide.content.h2}</h2>
+                  <p className="text-[30px]"> {slide.content.p}</p>
+                  <button className="btn mt-[10px]">Shop now</button>
+                </div>
+              </div>
+            </div>
+          );
+        }
+      })}
       {/* rightarrow div */}
       <div className={arrowStyle}>
-        <ArrowRightOutlined style={{ fontSize: "50px" }} />
+        <ArrowRightOutlined style={{ fontSize: "50px" }} onClick={nextSlide} />
       </div>
     </div>
   );
