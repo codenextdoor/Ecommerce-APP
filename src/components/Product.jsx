887012cf27204466df297f73b6298af31a8498c2
codenextdoor <xuluv911@gmail.com>
2022-12-22 17:28:28 +0545

    Newsletter Commit

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,30 +3,35 @@ import {
   FavoriteBorderOutlined,
   SearchOutlined,
 } from "@mui/icons-material";
-import {React, useState} from "react";
+import { React, useState } from "react";
 
 const Product = ({ item }) => {
+  const [hoverEffect, setHoverEffect] = useState("opacity-0");
 
-const [hoverEffect, setHoverEffect] = useState('opacity-0')
+  const handleHoverEnter = () => {
+    setHoverEffect(" opacity-1 bg-[rgba(0,0,0,0.2)]");
+  };
 
-const handleHoverEnter = () => {
-  setHoverEffect(' opacity-1 bg-[rgba(0,0,0,0.2)]')
-}
+  const handleHoverExit = () => {
+    setHoverEffect(" opacity-0 ");
+  };
 
-const handleHoverExit = () => {
-  setHoverEffect(' opacity-0 ')
-}
-  
-const iconStyle = 'h-[40px] w-[40px] rounded-full bg-white flex items-center justify-center m-3 cursor-pointer'
+  const iconStyle =
+    "h-[40px] w-[40px] rounded-full bg-white flex items-center justify-center m-3 cursor-pointer hover:bg-[#894af3] hover:text-white hover:scale-[1.1] ease-in duration-100 cursor-pointer";
 
   return (
-    <div className="flex flex-1 items-center justify-center min-w-[250px] min-h-[350px] overflow-hidden rounded-md shadow-lg m-2 relative"
-     onMouseEnter={handleHoverEnter}
-     onMouseLeave={handleHoverExit}
-
+    <div
+      className="flex flex-1 items-center justify-center min-w-[250px] min-h-[350px] overflow-hidden rounded-md shadow-lg m-2 relative"
+      onMouseEnter={handleHoverEnter}
+      onMouseLeave={handleHoverExit}
     >
       <img src={item.src} alt="product_image" />
-      <div className={`flex items-center justify-center w-[100%] h-[100%] absolute ease-in duration-100 ` + hoverEffect}>
+      <div
+        className={
+          `flex items-center justify-center w-[100%] h-[100%] absolute ease-in duration-100 ` +
+          hoverEffect
+        }
+      >
         {/* icons */}
         <div className={iconStyle}>
           <ShoppingCartOutlined />
