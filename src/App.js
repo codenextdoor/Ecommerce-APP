4cef89e57abef3fa0078f447087213123216cff0
codenextdoor <xuluv911@gmail.com>
2022-12-25 21:49:23 +0545

    Login commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 // import CategoryPage from "./pages/CategoryPage";
 import ProductPage from "./pages/ProductPage";
 // import Home from "./pages/Home";
@@ -7,7 +9,7 @@ import ProductPage from "./pages/ProductPage";
 const App = () => {
   return (
     <div>
-      <Cart />
+      <Register />
     </div>
   );
 };
---
+++ src/pages/Login.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+const [state, setState] = useState("");
+const Login = () => {
+  state = {
+    username: "",
+    pass: "",
+  };
+
+  const handleChange = (e) => {
+    const fields = Object.assign({}, state);
+    fields[e.target.name] = e.target.value;
+    setState({ ...fields });
+  };
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col absolute top-[30%] shadow-lg border-[2px] rounded-lg p-5 w-[50% ]">
+        <text className="text-2xl">Login</text>
+        <div className="flex mt-3">
+          <input
+            className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
+            type="text"
+            placeholder="username"
+            name="pass"
+            value={state.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <input
+            className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
+            type="password"
+            name="password"
+            placeholder="password"
+            value={state.pass}
+            required
+          />
+        </div>
+        <input
+          type="button"
+          value="Login"
+          className="btn mt-7 hover:scale-[1.02]"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
